8f1b3d8eea3b2e7067e96482923e16fcea6f2494
Phoebe Lau <phoebelau7@gmail.com>
2017-11-27 17:42:05 +0000

    automatically backed up by learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -1,5 +1,6 @@
+tatus = "rescued"
+
 def rescue_princess_peach
-  status = "rescued"
   puts "Hooray! Mario has rescued Princess Peach."
 end
 
@@ -7,10 +8,6 @@ end
 
 
 ## Comment the below back in to see method scope in action!
-
-# rescue_princess_peach
-# puts status
-
 
 rescue_princess_peach
 puts status
